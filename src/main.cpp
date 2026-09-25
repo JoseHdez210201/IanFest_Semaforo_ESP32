56c7bf57bfbc71604409ece9dc897dfd57495bd1
José <josehdez60306@gmail.com>
2026-09-24 23:18:33 -0600

    Feature: Secuencia de semáforo
    Se implementa una secuencia de encendido de 3 salidas digitales de forma ordenada imitando el funcionamiento básico de un semáforo

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,52 @@
 #include <Arduino.h>
 
-#define LED_PIN 2 // Pin del LED integrado en la mayoría de placas genéricas
+// Definimos los pines de salida digital
+#define RELAY_1_PIN 25 // Verde
+#define RELAY_2_PIN 26 // Amarillo
+#define RELAY_3_PIN 27 // Rojo
 
+// Declaración de prototipo de funciones
+void secuencia();
+void verde();
+void amarillo();
+void rojo();
+
+// Se configuran los pines como salida Open Drain, cuando mandas un HIGH en lugar de sacar 3.3V
+// el ESP32 simplemente "desconecta" el pin (lo deja en alta impedancia).
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  Serial.println("ESP32 inicializado correctamente.");
+  pinMode(RELAY_1_PIN, OUTPUT_OPEN_DRAIN);
+  pinMode(RELAY_2_PIN, OUTPUT_OPEN_DRAIN);
+  pinMode(RELAY_3_PIN, OUTPUT_OPEN_DRAIN);
 }
 
+// Se ejecuta de forma concurrente
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  secuencia();
+}
+
+void secuencia(){
+  verde();
+  delay(5000);
+  amarillo();
+  delay(5000);
+  rojo();
+  delay(5000);
+}
+
+void verde(){
+  digitalWrite(RELAY_1_PIN, LOW);
+  digitalWrite(RELAY_2_PIN, HIGH);
+  digitalWrite(RELAY_3_PIN, HIGH);
+}
+
+void amarillo(){
+  digitalWrite(RELAY_1_PIN, HIGH);
+  digitalWrite(RELAY_2_PIN, LOW);
+  digitalWrite(RELAY_3_PIN, HIGH);
+}
+
+void rojo(){
+  digitalWrite(RELAY_1_PIN, HIGH);
+  digitalWrite(RELAY_2_PIN, HIGH);
+  digitalWrite(RELAY_3_PIN, LOW);
 }
